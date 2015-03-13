5b18d879f44523c329cb39d361eadb51f2417cfc
antoine <ponsard.antoine@gmail.com>
2015-03-12 21:26:59 -0700

    Using nested columns and .img-responsive

--- js/controllers.js
+++ js/controllers.js
@@ -114,7 +114,7 @@ app.controller('aboutCtrl', ['$scope', function($scope){
    $scope.$on('$routeChangeSuccess', function () {
       // Resize the round images to have the size of the logo
       var width = $(".logo-col").width();
-      $(".round-image").width(width);
+/*      $(".round-image").width(width);*/
 
       // Resize the (empty) highlighting divs to have the size of the ones containing text
       var height = $(".highlighted.master").height();
